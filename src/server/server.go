e02c8a210b1744f3fa4318ce2ffd6093f618fb80
Hello Li <1261142605@qq.com>
2022-07-15 17:51:15 +0800

    广播上线用户消息

--- src/server/server.go
+++ src/server/server.go
@@ -2,7 +2,9 @@ package server
 
 import (
 	"fmt"
+	"go-IM/src/user"
 	"net"
+	"sync"
 )
 
 type Server struct {
@@ -10,12 +12,20 @@ type Server struct {
 	IP string
 	// 端口
 	Port int
+
+	OnlineMap map[string]*user.User
+	mapLock   sync.RWMutex
+
+	// 消息广播channel
+	Message chan string
 }
 
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		IP:   ip,
-		Port: port,
+		IP:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*user.User),
+		Message:   make(chan string),
 	}
 	return server
 }
@@ -29,6 +39,8 @@ func (server *Server) Start() {
 
 	// 程序结束前关闭服务
 	defer listener.Close()
+
+	go server.ListenMessager()
 
 	// 死循环获取链接状态
 	for {
@@ -45,5 +57,39 @@ func (server *Server) Start() {
 }
 
 func (server *Server) Handler(conn net.Conn) {
-	fmt.Println("链接成功")
+	// fmt.Println("链接成功")
+
+	user := user.NewUser(conn)
+
+	// 防止并发读写错误
+	server.mapLock.Lock()
+	server.OnlineMap[user.Name] = user
+	server.mapLock.Unlock()
+
+	server.BroadCast(user, "已上线")
+
+	// 阻塞当前handler
+	select {}
+}
+
+// 广播上线消息
+func (server *Server) BroadCast(user *user.User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + ":" + msg
+
+	// 往channel写数据
+	server.Message <- sendMsg
+}
+
+// goroutine 监听在线用户，当有消息过来时发送给所有用户
+func (server *Server) ListenMessager() {
+	for {
+		msg := <-server.Message
+
+		server.mapLock.Lock()
+		// 遍历OnlineMap 中搜集的user，向在线的user的channel发消息
+		for _, user := range server.OnlineMap {
+			user.C <- msg
+		}
+		server.mapLock.Unlock()
+	}
 }
---
+++ src/user/user.go
@@ -0,0 +1,36 @@
+package user
+
+import "net"
+
+type User struct {
+	Name string
+	Addr string
+	// channel
+	C    chan string
+	conn net.Conn
+}
+
+func NewUser(conn net.Conn) *User {
+	userAddr := conn.RemoteAddr().String()
+	user := &User{
+		Name: userAddr,
+		Addr: userAddr,
+		C:    make(chan string),
+		conn: conn,
+	}
+
+	// 启动goroutine监听当前user channel消息
+	go user.ListenMessage()
+
+	return user
+}
+
+func (user *User) ListenMessage() {
+	for {
+		// 从go程中获取到上线消息
+		msg := <-user.C
+
+		user.conn.Write([]byte(msg + "\n"))
+
+	}
+}
